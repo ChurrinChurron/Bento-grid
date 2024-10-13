39dfe75efed156828401f55caf40bbda59415baf
Mariana <81131232+ChurrinChurron@users.noreply.github.com>
2024-10-12 22:07:15 -0400

    heading scale

--- src/components/BoxTwo.tsx
+++ src/components/BoxTwo.tsx
@@ -9,7 +9,7 @@ const BoxTwo: React.FC<BoxTwoProps> = ({grid}) => {
     <div className={`rounded-md p-8 text-center bg-altPurple lg:content-center ${grid}`}>
       <h2 className="w-22 text-5xl font-medium text-white lg:text-6xl">Social Media <span className="text-yellow-400">10x</span> <span className="italic">Faster</span> with AI</h2>
       <img src={five_stars} className="h-8 mx-auto mt-6 mb-2" alt="five-stars" />
-      <h4 className="text-white text-lg">Over 4,000 5-stars reviews</h4>
+      <h3 className="text-white text-lg">Over 4,000 5-stars reviews</h3>
     </div>
   )
 }
